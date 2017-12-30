38f3a3807373a19cacc0171dd7d5fd11f44dbd35
troido <troido@hotmail.com>
2017-12-31 00:10:14 +0100

    merged roomedge into portal, moved tcommunicate and utils to common folder and fixed package when starting main directory

---
+++ asciifarm/common/utils.py
@@ -0,0 +1,35 @@
+
+import os
+
+def clamp(val, lower, upper):
+    """ val if it's between lower and upper, else the closest of the two"""
+    return min(max(val, lower), upper)
+
+
+def concat(arr):
+    """Takes a list of sequences, returns the concatenation of the sequences """
+    if isinstance(arr[0], str):
+        return "".join(arr)
+    if isinstance(arr[0], bytes):
+        return b"".join(arr)
+    if isinstance(arr[0], list):
+        l = []
+        for s in arr:
+            l += s
+        return l
+    if isinstance(arr[0], tuple):
+        l = []
+        for s in arr:
+            l += s
+        return tuple(l)
+    else:
+        raise ValueError("type {} can't be concatenated".format(type(arr[0])))
+
+
+def writeFileSafe(filename, data, tempname=None):
+    if tempname == None:
+        tempname = filename + ".tempfile"
+    with open(tempname, 'w') as f:
+        f.write(data)
+    os.rename(tempname, filename)
+    
